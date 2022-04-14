b22e9cd951f00cca2ee60ee297ba69cf85d03b02
Tomd304 <tomd304@live.co.uk>
2022-04-14 16:46:11 +0100

    Authentication full stack setup

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import WebPlayback from "./WebPlayback";
+import Login from "./Login";
+import "./App.css";
+
+function App() {
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    async function getToken() {
+      const response = await fetch("http://localhost:5000/auth/token");
+      const json = await response.json();
+      setToken(json.access_token);
+    }
+
+    getToken();
+  }, []);
+  console.log(token);
+  return <>{token === "" ? <Login /> : <WebPlayback token={token} />}</>;
+}
+
+export default App;
---
+++ src/WebPlayback.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function WebPlayback(props) {
+  return (
+    <div className="App">
+      <header className="App-header">Logged in</header>
+      console.log(props.token)
+    </div>
+  );
+}
+
+export default WebPlayback;
