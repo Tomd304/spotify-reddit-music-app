4d1aba70c685482473af2643c02ef94434270bb3
Tomd304 <tomd304@live.co.uk>
2022-04-18 15:43:09 +0100

    Updates state and page results on button click

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,6 +2,12 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 function Dashboard(props) {
+  const [searchOps, setSearchOps] = useState({
+    q: "album",
+    t: "all",
+    sort: "relevance",
+  });
+
   useEffect(() => {
     const redditGet = async (q, t, sort) => {
       const res = await fetch(
@@ -15,67 +21,71 @@ function Dashboard(props) {
       const json = await res.json();
       setMusicItems(json.results);
     };
-    redditGet("album", "all", "relevance");
-  }, []);
+    redditGet(searchOps.q, searchOps.t, searchOps.sort);
+  }, [searchOps]);
+
+  const searchSubmit = (e) => {
+    e.preventDefault();
+    setSearchOps({
+      q: e.target[0].value,
+      t: e.target[1].value,
+      sort: e.target[2].value,
+    });
+  };
 
   const [musicItems, setMusicItems] = useState([]);
-
-  console.log(musicItems);
-
+  console.log(searchOps);
   return (
-    <ul>
-      {musicItems.map((item) => {
-        return <li>{item}</li>;
-      })}
-    </ul>
+    <div>
+      <form onSubmit={searchSubmit}>
+        <select name="q">
+          <option name="album" value="album">
+            Album
+          </option>
+          <option name="song" value="song">
+            Song
+          </option>
+        </select>
+        <select name="t">
+          <option name="all" value="all">
+            All
+          </option>
+          <option name="year" value="year">
+            Year
+          </option>
+          <option name="month" value="month">
+            Month
+          </option>
+          <option name="week" value="week">
+            Week
+          </option>
+          <option name="day" value="day">
+            Day
+          </option>
+        </select>
+        <select name="sort">
+          <option name="relevance" value="relevance">
+            Relevance
+          </option>
+          <option name="hot" value="hot">
+            Hot
+          </option>
+          <option name="top" value="top">
+            Top
+          </option>
+          <option name="new" value="new">
+            New
+          </option>
+        </select>
+        <button>Update</button>
+      </form>
+      <ul>
+        {musicItems.map((item) => {
+          return <li>{item}</li>;
+        })}
+      </ul>
+    </div>
   );
 }
-
-const searchPanel = (
-  <div>
-    <form>
-      <select name="q">
-        <option name="album" value="album">
-          Album
-        </option>
-        <option name="song" value="song">
-          Song
-        </option>
-      </select>
-      <select name="t">
-        <option name="all" value="all">
-          All
-        </option>
-        <option name="year" value="year">
-          Year
-        </option>
-        <option name="month" value="month">
-          Month
-        </option>
-        <option name="week" value="week">
-          Week
-        </option>
-        <option name="day" value="day">
-          Day
-        </option>
-      </select>
-      <select name="sort">
-        <option name="relevance" value="relevance">
-          Relevance
-        </option>
-        <option name="hot" value="hot">
-          Hot
-        </option>
-        <option name="top" value="top">
-          Top
-        </option>
-        <option name="new" value="new">
-          New
-        </option>
-      </select>
-      <button>Update</button>
-    </form>
-  </div>
-);
 
 export default Dashboard;
