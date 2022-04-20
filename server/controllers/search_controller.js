c565232c3fd8d0d4899e202bcbc0382e9323df76
Tomd304 <tomd304@live.co.uk>
2022-04-20 17:57:18 +0100

    search controller now scans reddit for both spotify items and plain text items. items are filtered on type and api requests made to spotify

--- server/controllers/search_controller.js
+++ server/controllers/search_controller.js
@@ -1,7 +1,10 @@
 const requestPromise = require("request-promise");
 const globalVal = require("../globalVariables");
+const he = require("he");
+const { format: prettyFormat } = require("pretty-format");
 
 const searchReddit = async (q, t, sort) => {
+  const searchType = q == "album" ? "album" : "track";
   q =
     q == "album"
       ? '"FRESH ALBUM" OR "FRESH EP" OR "FRESH MIXTAPE"'
@@ -12,7 +15,7 @@ const searchReddit = async (q, t, sort) => {
     sort: sort,
     t: t,
     restrict_sr: 1,
-    limit: 20,
+    limit: 15,
     after: "after",
   };
 
@@ -24,13 +27,33 @@ const searchReddit = async (q, t, sort) => {
   };
 
   const res = JSON.parse(await requestPromise(options)).data.children;
-  const redditURLS = res
-    .filter((child) => child.data.url.includes("open.spotify.com"))
-    .map((child) => {
-      return extractIDandType(child.data.url);
-    });
+  const testURLS = parseRedditAlbums(res, searchType).filter(
+    (child) => child.score > 5
+  );
 
-  return redditURLS;
+  return testURLS;
+};
+
+const parseRedditAlbums = (list, searchType) => {
+  const results = list.map((child) => {
+    let type, data;
+    if (child.data.url.includes("open.spotify.com")) {
+      type = "spotify";
+      data = extractIDandType(child.data.url);
+    } else {
+      type = "text";
+      data = he.decode(child.data.title.split("]").pop()).replace("-", " ");
+    }
+    return {
+      type,
+      data,
+      title: he.decode(child.data.title),
+      timestamp: child.data.created,
+      score: child.data.score,
+      searchType,
+    };
+  });
+  return results;
 };
 
 const extractIDandType = (str) => {
@@ -49,11 +72,8 @@ const extractIDandType = (str) => {
 };
 
 exports.getItems = async (req, res) => {
-  console.log("*********************");
-  console.log("api call");
-  console.log("*********************");
-  params = req.query;
-  results = await searchReddit(params.q, params.t, params.sort);
+  const params = req.query;
+  const results = await searchReddit(params.q, params.t, params.sort);
   const details = await getSpotDetails(results);
   res.json({
     results: details,
@@ -61,9 +81,22 @@ exports.getItems = async (req, res) => {
 };
 
 const getSpotDetails = async (urlList) => {
-  var url = "https://api.spotify.com/v1/albums/?ids=";
-  urlList.forEach((item) => {
-    url += item.split("/albums/")[1] + ",";
+  const albums = await getSpotAlbums(
+    urlList.filter((item) => item.type == "spotify")
+  );
+  const searches = await getSpotSearches(
+    urlList.filter((item) => item.type == "text")
+  );
+  console.log("***********ALBUMS*****************");
+  console.log(prettyFormat(albums));
+  console.log("***********SEARCHES*****************");
+  console.log(prettyFormat(searches));
+};
+
+const getSpotAlbums = async (albumList) => {
+  let url = "https://api.spotify.com/v1/albums/?ids=";
+  albumList.forEach((item) => {
+    url += item.data.split("/albums/")[1] + ",";
   });
   url = url.slice(0, -1);
   const options = {
@@ -75,12 +108,41 @@ const getSpotDetails = async (urlList) => {
     },
   };
   const res = JSON.parse(await requestPromise(options));
-  details = res.albums.map((item) => {
+  return albumList.map(function (item, i) {
     return {
-      name: item.name,
-      image: item.images[0].url,
-      artist: item.artists[0].name,
+      ...item,
+      name: res.albums[i].name,
+      image: res.albums[i].images[0].url,
+      artist: res.albums[i].artists[0].name,
     };
   });
-  return details;
+};
+
+const getSpotSearches = async (searchList) => {
+  console.table(searchList);
+  let spotResults = await Promise.all(
+    searchList.map(async (item) => {
+      let url = `https://api.spotify.com/v1/search?q=${encodeURI(
+        item.data.trim()
+      )}&type=${item.searchType}`;
+      let options = {
+        url,
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + globalVal.access_token,
+        },
+      };
+      const res = JSON.parse(await requestPromise(options));
+      if (res.albums.items[0]) {
+        return {
+          ...item,
+          name: res.albums.items[0].name,
+          image: res.albums.items[0].images[0].url,
+          artist: res.albums.items[0].artists[0].name,
+        };
+      }
+    })
+  );
+  return spotResults;
 };
