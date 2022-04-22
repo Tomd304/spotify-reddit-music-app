1de1155ce1125c8298d597c771967d4b168a96d9
Tomd304 <tomd304@live.co.uk>
2022-04-22 13:51:55 +0100

    Styling added for links

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,15 @@ import "./Card.css";
 const Card = (props) => {
   return (
     <div className="card">
-      <img src={props.item.image} className="card-img" />
-      <h1>{props.item.artist.name}</h1>
-      <p>{props.item.name}</p>
+      <a href={props.item.album.url}>
+        <img src={props.item.image} className="card-img" />
+      </a>
+      <a href={props.item.artist.url}>
+        <h1>{props.item.artist.name}</h1>
+      </a>
+      <a href={props.item.url}>
+        <p>{props.item.name}</p>
+      </a>
     </div>
   );
 };
